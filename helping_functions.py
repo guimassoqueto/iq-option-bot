e2e25bfb36943da7106112529c6d932b1a7ae569
Guilherme <gui.massoqueto@gmail.com>
2022-01-13 08:11:41 -0300

    code changes

--- helping_functions.py
+++ helping_functions.py
@@ -5,6 +5,9 @@ from iqoptionapi.stable_api import IQ_Option
 from os import environ
 import threading
 from datetime import datetime
+from multiprocessing import cpu_count, Process
+from iqoptionapi.constants import ACTIVES
+from time import time
 
 def login_IQ_Option():
     return IQ_Option(environ['MY_EMAIL'], environ['IQ_OPTION_PWD'])
@@ -16,11 +19,6 @@ def consecutive_up(last_five: list) -> bool:
 def consecutive_down(last_five: list) -> bool:
     [c5, c4, c3, c2, c1] = last_five
     return c5['open'] > c5['close'] and c4['open'] > c4['close'] and c3['open'] > c3['close'] and c2['open'] > c2['close'] and c1['open'] > c1['close']
-
-def write_delay(active: str, timeframe: int, action: str, delay: float) -> None:
-    with open('entry_delays.csv', 'a', encoding='utf-8', newline='') as csvfile:
-        spamwriter = writer(csvfile)
-        spamwriter.writerow([active, timeframe, action, delay])
 
 def write_consecutives(active: str, up_or_down: str) -> None:
     now = datetime.now().strftime("%d/%m/%y %H:%M:%S")
@@ -35,3 +33,55 @@ def set_interval(func, sec):
     t = threading.Timer(sec, func_wrapper)
     t.start()
     return t
+
+def all_tradeable_actives() -> list:
+    '''Return a list of IQ Options' actives able to trade'''
+    return [active for active in ACTIVES.keys()]
+
+def get_actives_to_trade() -> list:
+    '''
+    Get user's actives that he want to trade. 1 for CPU.
+    '''
+    all_actives = all_tradeable_actives()
+    loop = True
+    actives = []
+    count = 1 # cpu_count()
+
+    while loop:
+        active = input(f"Active ({count} left): ").upper()
+        
+        if active not in all_actives: 
+            active = input(f"Active ({count} left): ").upper()
+        else:
+            if active not in actives:
+                actives.append(active)
+                count -= 1
+            else: continue
+        
+        if count == 0: loop = False
+        
+    return actives
+
+def get_timeframe_to_trade() -> int:
+    '''
+        Creates the timeframe based on user' timeframe selection
+    '''
+    timeframe = input('Timeframe (30, 60, 120, 300): ')
+
+    while timeframe not in ('30', '60', '120', '300'):
+        timeframe = input('Timeframe (30, 60, 120, 300): ')
+
+    return int(timeframe)
+
+def generate_processes(actives_list: list, func, iq: object) -> dict:
+    '''
+    Generates an process dictionary of all the user's chosen actives
+    '''
+    timeframe = get_timeframe_to_trade()
+
+    processes = {}
+
+    for active in actives_list:
+        processes[active] = Process(target=func, args=(iq, active, timeframe, 6))
+
+    return processes
--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 #! /usr/bin/python3
 
-from time import time
-from helping_functions import login_IQ_Option, consecutive_down, consecutive_up, write_consecutives
-from all_tradeable_actives import all_tradeable_actives
+from time import time, sleep
+from helping_functions import login_IQ_Option, consecutive_down, consecutive_up, all_tradeable_actives
 from variables import expiration_mode
 
+all_act = all_tradeable_actives()
+
 ACTIVE = input('Active: ').upper()
-while ACTIVE not in all_tradeable_actives:
+while ACTIVE not in all_act:
     ACTIVE = input('Active: ').upper()
 
 TIMEFRAME = int(input('Timeframe (30, 60, 120 or 300): '))
 while TIMEFRAME not in (30, 60, 120, 300):
     TIMEFRAME = int(input('Timeframe (30, 60, 120 or 300): '))
 
-EXPIRATION_MODE = expiration_mode[str(TIMEFRAME)]
+EXPIRATION = expiration_mode[str(TIMEFRAME)]
 
 iqoption = login_IQ_Option()
 check, reason = iqoption.connect()
@@ -32,9 +33,260 @@ if check:
             arr.append(current['to'])
             arr = arr[1:]
 
-            if consecutive_up(last_candles): 
-                write_consecutives(ACTIVE, 'UP')
+            if consecutive_up(last_candles):
+                loop = False
+                action = 'put'
+                balance = BALANCE * 0.0001
+                success1, id1 = iqoption.buy(balance, ACTIVE, action, EXPIRATION)
 
+                while not success1:
+                    success1, id1 = iqoption.buy(balance, ACTIVE, action, EXPIRATION)
+
+                print('Wait for results (PUT)...')
+
+                status1, profit1 = iqoption.check_win_v3(id1)
+
+                if(status1 == 'loose'):
+                    success2, id2 = iqoption.buy(balance * 2.5, ACTIVE, action, EXPIRATION)
+
+                    while not success2:
+                        success2, id2 = iqoption.buy(balance * 2.5, ACTIVE, action, EXPIRATION)
+                    print('M2')
+                    status2, profit2 = iqoption.check_win_v3(id2)
+
+                    if(status2 == 'loose'):
+                        success3, id3 = iqoption.buy(balance * 2.5 ** 2, ACTIVE, action, EXPIRATION)
+
+                        while not success3:
+                            success3, id3 = iqoption.buy(balance * 2.5 ** 2, ACTIVE, action, EXPIRATION)
+                        print('M3')
+                        status3, profit3 = iqoption.check_win_v3(id3)
+
+                        if(status3 == 'loose'):
+                            success4, id4 = iqoption.buy(balance * 2.5 ** 3, ACTIVE, action, EXPIRATION)
+
+                            while not success4:
+                                success4, id4 = iqoption.buy(balance * 2.5 ** 3, ACTIVE, action, EXPIRATION)
+                            print('M4')
+                            status4, profit4 = iqoption.check_win_v3(id4)
+
+                            if(status4 == 'loose'):
+                                success5, id5 = iqoption.buy(balance * 2.5 ** 4, ACTIVE, action, EXPIRATION)
+
+                                while not success5:
+                                    success5, id5 = iqoption.buy(balance * 2.5 ** 4, ACTIVE, action, EXPIRATION)
+                                print('M5')
+                                status5, profit5 = iqoption.check_win_v3(id5)
+
+                                if(status5 == 'loose'):
+                                    success6, id6 = iqoption.buy(balance * 2.5 ** 5, ACTIVE, action, EXPIRATION)
+
+                                    while not success6:
+                                        success6, id6 = iqoption.buy(balance * 2.5 ** 5, ACTIVE, action, EXPIRATION)
+                                    print('M6')
+                                    status6, profit6 = iqoption.check_win_v3(id6)
+
+                                    if(status6 == 'loose'):
+                                        success7, id7 = iqoption.buy(balance * 2.5 ** 6, ACTIVE, action, EXPIRATION)
+                                        print('M7')
+                                        while not success7:
+                                            success7, id7 = iqoption.buy(balance * 2.5 ** 6, ACTIVE, action, EXPIRATION)
+                                        
+                                        status7, profit7 = iqoption.check_win_v3(id7)
+
+                                        if (status7 == 'loose'):
+                                            print(f"You lose ${profit1 + profit2 + profit3 + profit4 + profit5 + profit6 + profit7}")
+                                            BALANCE = iqoption.get_balance()
+                                            loop = True
+                                        elif(status7 == 'win'):
+                                            print(f"You win ${profit7 + profit6 + profit5 + profit4 + profit3 + profit2 + profit1}")
+                                            BALANCE = iqoption.get_balance()
+                                            loop = True
+                                        else:
+                                            print(f"DRAW")
+                                            sleep(600)
+                                            BALANCE = iqoption.get_balance()
+                                            loop = True
+                                    elif(status6 == 'win'):
+                                        print(f"You win ${profit6 + profit5 + profit4 + profit3 + profit2 + profit1}")
+                                        BALANCE = iqoption.get_balance()
+                                        loop = True
+                                    else:
+                                        print(f"DRAW")
+                                        sleep(600)
+                                        BALANCE = iqoption.get_balance()
+                                        loop = True
+                                elif(status5 == 'win'):
+                                    print(f"You win ${profit5 + profit4 + profit3 + profit2 + profit1}")
+                                    BALANCE = iqoption.get_balance()
+                                    loop = True
+                                else:
+                                    print(f"DRAW")
+                                    sleep(600)
+                                    BALANCE = iqoption.get_balance()
+                                    loop = True
+                            elif(status4 == 'win'):
+                                print(f"You win ${profit4 + profit3 + profit2 + profit1}")
+                                BALANCE = iqoption.get_balance()
+                                loop = True
+                            else:
+                                print(f"DRAW")
+                                sleep(600)
+                                BALANCE = iqoption.get_balance()
+                                loop = True
+                        elif(status3 == 'win'):
+                            print(f"You win ${profit3 + profit2 + profit1}")
+                            BALANCE = iqoption.get_balance()
+                            loop = True
+                        else:
+                            print(f"DRAW")
+                            sleep(600)
+                            BALANCE = iqoption.get_balance()
+                            loop = True
+                    elif(status2 == 'win'):
+                        print(f"You win ${profit2 + profit1}")
+                        BALANCE = iqoption.get_balance()
+                        loop = True
+                    else:
+                        print(f"DRAW")
+                        sleep(600)
+                        BALANCE = iqoption.get_balance()
+                        loop = True
+                elif(status1 == 'win'):
+                    print(f"You win ${profit1}")
+                    BALANCE = iqoption.get_balance()
+                    loop = True
+                else:
+                    print(f"DRAW")
+                    sleep(600)
+                    BALANCE = iqoption.get_balance()
+                    loop = True
+                                            
             if consecutive_down(last_candles):
-                write_consecutives(ACTIVE, 'DOWN')
-        
+                loop = False
+                action = 'call'
+                balance = BALANCE * 0.0001
+                success1, id1 = iqoption.buy(balance, ACTIVE, action, EXPIRATION)
+
+                while not success1:
+                    success1, id1 = iqoption.buy(balance, ACTIVE, action, EXPIRATION)
+
+                print("Wait for results (CALL)...")
+
+                status1, profit1 = iqoption.check_win_v3(id1)
+
+                if(status1 == 'loose'):
+                    success2, id2 = iqoption.buy(balance * 2.5, ACTIVE, action, EXPIRATION)
+
+                    while not success2:
+                        success2, id2 = iqoption.buy(balance * 2.5, ACTIVE, action, EXPIRATION)
+                    print('M2')
+                    status2, profit2 = iqoption.check_win_v3(id2)
+
+                    if(status2 == 'loose'):
+                        success3, id3 = iqoption.buy(balance * 2.5 ** 2, ACTIVE, action, EXPIRATION)
+
+                        while not success3:
+                            success3, id3 = iqoption.buy(balance * 2.5 ** 2, ACTIVE, action, EXPIRATION)
+                        print('M3')
+                        status3, profit3 = iqoption.check_win_v3(id3)
+
+                        if(status3 == 'loose'):
+                            success4, id4 = iqoption.buy(balance * 2.5 ** 3, ACTIVE, action, EXPIRATION)
+
+                            while not success4:
+                                success4, id4 = iqoption.buy(balance * 2.5 ** 3, ACTIVE, action, EXPIRATION)
+                            print('M4')
+                            status4, profit4 = iqoption.check_win_v3(id4)
+
+                            if(status4 == 'loose'):
+                                success5, id5 = iqoption.buy(balance * 2.5 ** 4, ACTIVE, action, EXPIRATION)
+
+                                while not success5:
+                                    success5, id5 = iqoption.buy(balance * 2.5 ** 4, ACTIVE, action, EXPIRATION)
+                                print('M5')
+                                status5, profit5 = iqoption.check_win_v3(id5)
+
+                                if(status5 == 'loose'):
+                                    success6, id6 = iqoption.buy(balance * 2.5 ** 5, ACTIVE, action, EXPIRATION)
+
+                                    while not success6:
+                                        success6, id6 = iqoption.buy(balance * 2.5 ** 5, ACTIVE, action, EXPIRATION)
+                                    print('M6')
+                                    status6, profit6 = iqoption.check_win_v3(id6)
+
+                                    if(status6 == 'loose'):
+                                        success7, id7 = iqoption.buy(balance * 2.5 ** 6, ACTIVE, action, EXPIRATION)
+
+                                        while not success7:
+                                            success7, id7 = iqoption.buy(balance * 2.5 ** 6, ACTIVE, action, EXPIRATION)
+                                        print('M7')
+                                        status7, profit7 = iqoption.check_win_v3(id7)
+
+                                        if (status7 == 'loose'):
+                                            print(f"You lose ${profit1 + profit2 + profit3 + profit4 + profit5 + profit6 + profit7}")
+                                            BALANCE = iqoption.get_balance()
+                                            loop = True
+                                        elif(status7 == 'win'):
+                                            print(f"You win ${profit7 + profit6 + profit5 + profit4 + profit3 + profit2 + profit1}")
+                                            BALANCE = iqoption.get_balance()
+                                            loop = True
+                                        else:
+                                            print(f"DRAW")
+                                            sleep(300)
+                                            BALANCE = iqoption.get_balance()
+                                            loop = True
+                                    elif(status6 == 'win'):
+                                        print(f"You win ${profit6 + profit5 + profit4 + profit3 + profit2 + profit1}")
+                                        BALANCE = iqoption.get_balance()
+                                        loop = True
+                                    else:
+                                        print(f"DRAW")
+                                        sleep(300)
+                                        BALANCE = iqoption.get_balance()
+                                        loop = True
+                                elif(status5 == 'win'):
+                                    print(f"You win ${profit5 + profit4 + profit3 + profit2 + profit1}")
+                                    BALANCE = iqoption.get_balance()
+                                    loop = True
+                                else:
+                                    print(f"DRAW")
+                                    sleep(300)
+                                    BALANCE = iqoption.get_balance()
+                                    loop = True
+                            elif(status4 == 'win'):
+                                print(f"You win ${profit4 + profit3 + profit2 + profit1}")
+                                BALANCE = iqoption.get_balance()
+                                loop = True
+                            else:
+                                print(f"DRAW")
+                                sleep(300)
+                                BALANCE = iqoption.get_balance()
+                                loop = True
+                        elif(status3 == 'win'):
+                            print(f"You win ${profit3 + profit2 + profit1}")
+                            BALANCE = iqoption.get_balance()
+                            loop = True
+                        else:
+                            print(f"DRAW")
+                            sleep(300)
+                            BALANCE = iqoption.get_balance()
+                            loop = True
+                    elif(status2 == 'win'):
+                        print(f"You win ${profit2 + profit1}")
+                        BALANCE = iqoption.get_balance()
+                        loop = True
+                    else:
+                        print(f"DRAW")
+                        sleep(300)
+                        BALANCE = iqoption.get_balance()
+                        loop = True
+                elif(status1 == 'win'):
+                    print(f"You win ${profit1}")
+                    BALANCE = iqoption.get_balance()
+                    loop = True
+                else:
+                    print(f"DRAW")
+                    sleep(300)
+                    BALANCE = iqoption.get_balance()
+                    loop = True
